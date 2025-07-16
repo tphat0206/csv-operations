1d44d542d70221f6e7e63482f751404c80d59081
tphat0206 <tphat12a@gmail.com>
2025-07-17 00:04:44 +0700

    simple admin page

--- account/models/account.py
+++ account/models/account.py
@@ -1,11 +1,26 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from account.models.base import BaseModel
 
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        # The key here is what arguments you define for create_superuser
+        if not email:
+            raise ValueError('The Email field must be set')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class Account(BaseModel, AbstractUser):
     email = models.EmailField(unique=True)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- file_service/admin.py
+++ file_service/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UploadedFile, FileOperation
 
-# Register your models here.
+admin.site.register(UploadedFile)
+admin.site.register(FileOperation)
